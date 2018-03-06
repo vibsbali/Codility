8f7ce827b00225eb77fa7ca1085a167a1e6560a3
Vaibhav Bali <vibsbali@gmail.com>
2018-03-06 17:34:24 +1100

    Week two exercises

---
+++ Codility.WeekTwo.Pair/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+ *
+ *
+ * A non-empty zero-indexed array A consisting of N integers is given. The array contains an odd number of elements, and each element of the array can be paired with another element that has the same value, except for one element that is left unpaired.
+
+For example, in array A such that:
+
+  A[0] = 9  A[1] = 3  A[2] = 9
+  A[3] = 3  A[4] = 9  A[5] = 7
+  A[6] = 9
+the elements at indexes 0 and 2 have value 9,
+the elements at indexes 1 and 3 have value 3,
+the elements at indexes 4 and 6 have value 9,
+the element at index 5 has value 7 and is unpaired.
+Write a function:
+
+class Solution { public int solution(int[] A); }
+
+that, given an array A consisting of N integers fulfilling the above conditions, returns the value of the unpaired element.
+
+For example, given array A such that:
+
+  A[0] = 9  A[1] = 3  A[2] = 9
+  A[3] = 3  A[4] = 9  A[5] = 7
+  A[6] = 9
+the function should return 7, as explained in the example above.
+
+Assume that:
+
+N is an odd integer within the range [1..1,000,000];
+each element of array A is an integer within the range [1..1,000,000,000];
+all but one of the values in A occur an even number of times.
+Complexity:
+
+expected worst-case time complexity is O(N);
+expected worst-case space complexity is O(1), beyond input storage (not counting the storage required for input arguments).
+Copyright 2009–2018 by Codility Limited. All Rights Re
+ */
+namespace Codility.WeekTwo.Pair
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var sol = new Solution();
+            Console.WriteLine(sol.solution(new []{9, 3, 9, 3, 9, 9, 9}));
+        }
+    }
+
+    class Solution
+    {
+        public int solution(int[] A)
+        {
+            // write your code in C# 6.0 with .NET 4.5 (Mono)
+            if (A.Length == 0)
+            {
+                return 0;
+            }
+
+            var hashTable = new Dictionary<int, int>();
+            var sum = 0;
+            for (int i = 0; i < A.Length; i++)
+            {
+                if (hashTable.ContainsKey(A[i]))
+                {
+                    hashTable.Remove(A[i]);
+                    sum -= A[i];
+                }
+                else
+                {
+                    hashTable.Add(A[i], 0);
+                    sum += A[i];
+                }
+            }
+
+            return sum;
+        }
+    }
+}
---
+++ Codility.WeekTwo.ReverseAnArray/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codility.WeekTwo.ReverseAnArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var one = "Hello World";
+            var enumerable = one.Reverse();
+            foreach (var item in enumerable)
+            {
+                Console.Write($"{item} ");
+            }
+        }
+    }
+
+    public static class ReverseArray
+    {
+        public static void Reverse<T>(this T[] A)
+        {
+            if (A.Length <= 1)
+            {
+                return;
+            }
+
+            var ptrOne = 0;
+            var ptrTwo = A.Length - 1;
+
+            while (ptrOne < ptrTwo)
+            {
+                var temp = A[ptrOne];
+                A[ptrOne] = A[ptrTwo];
+                A[ptrTwo] = temp;
+                ++ptrOne;
+                --ptrTwo;
+            }
+        }
+    }
+}
